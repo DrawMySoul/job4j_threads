0b94faf6d92c03302d99e1a021bc01fbe7f18621
Michael Lobov <michael.lobov13@gmail.com>
2022-12-12 23:49:20 +0300

    8. Скачивание файла с ограничением. [#144271]

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,67 @@
+package ru.job4j.thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        String fileName = url.substring(url.lastIndexOf('/') + 1);
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long delta = System.currentTimeMillis() - start;
+                if (bytesRead >= speed) {
+                    Thread.sleep(1000 * (bytesRead / speed - delta));
+                }
+                start = System.currentTimeMillis();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void urlValidate(String url) {
+        if (!url.matches("^https?://(?:www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$")) {
+            throw new IllegalArgumentException("Invalid URL");
+        }
+    }
+
+    private static void speedValidate(String speed) {
+        if (Integer.parseInt(speed) <= 0) {
+            throw new IllegalArgumentException("Invalid speed! Value must be more than zero");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Args are empty! Parameters: first is url, second is speed limit");
+        } else if (args.length == 1) {
+            throw new IllegalArgumentException("Args contains only one parameter! Parameters: first is url, second is speed limit ");
+        } else {
+            urlValidate(args[0]);
+            speedValidate(args[1]);
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
