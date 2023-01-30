784f9f4f41a90b9a0db95a67c0e6f931a0a5f66d
Michael Lobov <michael.lobov13@gmail.com>
2023-01-30 20:39:53 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -41,6 +43,43 @@ class SimpleBlockingQueueTest {
         producer.join();
         consumer.join();
         assertThat(expected).isNotEmpty()
+            .startsWith(0)
+            .endsWith(9)
+            .containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
+
+    @Test
+    void whenOfferAndPollParallelSearch() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(2);
+        Thread producer = new Thread(
+            () -> IntStream.range(0, 10).forEach(
+                value -> {
+                    try {
+                        queue.offer(value);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            )
+        );
+        Thread consumer = new Thread(
+            () -> {
+                while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                    try {
+                        buffer.add(queue.poll());
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).isNotEmpty()
             .startsWith(0)
             .endsWith(9)
             .containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
